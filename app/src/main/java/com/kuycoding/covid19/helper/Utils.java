33d6dfcbd6241a6f86a1b8ca80961b98437963fc
Kuy CODING <46470594+kuycoding@users.noreply.github.com>
2020-04-25 13:09:26 +0700

    Update Utils.java

--- app/src/main/java/com/kuycoding/covid19/helper/Utils.java
+++ app/src/main/java/com/kuycoding/covid19/helper/Utils.java
@@ -4,5 +4,5 @@ public class Utils {
     //This is your from email
     public static final String EMAIL = "info@covid19.kuycoding.com";
     //This is your from email password
-    public static final String PASSWORD = "afif11win17";
+    public static final String PASSWORD = "passwd";
 }
